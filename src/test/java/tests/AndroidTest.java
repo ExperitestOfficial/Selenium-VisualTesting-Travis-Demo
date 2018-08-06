a268efa9dcb88fdb15e95a5ce947e4fcfd02fbe6
Jonathan Aharon <jonathan.aharon_p@experitest.com>
2018-08-06 11:17:02 +0300

    message

--- src/test/java/tests/AndroidTest.java
+++ src/test/java/tests/AndroidTest.java
@@ -47,6 +47,7 @@ public class AndroidTest {
         driver.get("https://github.com/login");
         //new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@nodeName='BUTTON' and (./preceding-sibling::* | ./following-sibling::*)[@text]]")));
         Thread.sleep(3000);
+        
         in.Visual.setAccessKey(VISUAL_ACCESS_KEY);
         boolean result  = in.Visual.verify(driver, "github_login_page");
         System.out.println("Report Link: " + in.Visual.getLastResultUrl());
